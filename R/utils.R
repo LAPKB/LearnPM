82d8bbee946eb9dae8f572dfdd75672ac02bf938
Michael Neely <mneely@usc.edu>
2023-06-29 13:57:43 -0700

    Building tutorial

--- R/utils.R
+++ R/utils.R
@@ -15,16 +15,28 @@
 
 #' @export
 pkgPDF <- function(file) {
-  system(paste0("open ",
-                system.file(paste0("tutorials/data/",file), package = "LearnPM")
-                )
-  )
+  # system(paste0("open ",
+  #               system.file(paste0("tutorials/data/",file), package = "LearnPM")
+  #               )
+  # )
+  #
+  system(paste0("open data/",file))
 }
 
 #' @export
-install_Pmetrics <- function(ref = "HEAD", force = FALSE){
+install_Pmetrics <- function(ref = "HEAD", force = FALSE, ask = FALSE){
 
   if(!rlang::is_installed("Pmetrics") | force){
+    if(ask){
+      cat("The Pmetrics package will be dowloaded from Github and installed on your computer.\n")
+      cat("OK to proceed?\n\n")
+      ans <- readline("1. Yes, install current version.\n2. Yes, install beta version.\n3. No, do not install.\n")
+      if(ans == 2){ ref = "dev"}
+      if(ans == 3){
+        cat("Installation aborted.\n")
+        return(invisible(NULL))
+      }
+    }
     remotes::install_github("LAPKB/Pmetrics", ref = ref, force = force)
     cat(crayon::red("Important:"), "Don't forget to run",crayon::blue("PMbuild()"),
         "after the package is installed.")
